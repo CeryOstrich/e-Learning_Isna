1accec537f087573fbd315e035e4f04e2a99e219
Marvx-UI <rezkygf8055@gmail.com>
2026-06-01 19:45:23 +0800

    fix Akses

--- Isnun/proses/hapus_live_class.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../database.php";
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM live_class WHERE id='$id'");
-header("Location: ../dashboard.php?page=live_class_guru");
-exit;
-?>
